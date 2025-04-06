8e234207d36c0334982dda884e5b819f2b225f7a
Alexander Anderson <AlexanderAndersonProgramming@gmail.com>
2025-04-06 16:03:39 -0500

    Talk about colors in bash

---
+++ bash_scripting_series/color_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. ./bash_colors.sh
+
+
+echo -e "${BLACK}black${NC}<-black"
+echo -e "${GRAY}gray"
+echo -e "${LIGHT_GRAY}light gray"
+echo -e "${WHITE}white"
+echo -e "${RED}red"
+echo -e "${BR_RED}bright red"
+echo -e "${DARK_YELLOW}dark yellow"
+echo -e "${YELLOW}yellow"
+echo -e "${BR_GREEN}bright green"
+echo -e "${GREEN}green"
+echo -e "${LIGHT_CYAN}light cyan"
+echo -e "${CYAN}cyan"
+echo -e "${BLUE}blue"
+echo -e "${DARK_BLUE}dark blue"
+echo -e "${PINK}pink"
+echo -e "${PURPLE}purple"
+
